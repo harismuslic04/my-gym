67f29502328d57110e0e940f6107d59f363abd4a
harismuslic04 <harismuslic16@gmail.com>
2024-10-09 22:54:21 +0200

    a

--- src/components/ExerciseCard.jsx
+++ src/components/ExerciseCard.jsx
@@ -22,7 +22,9 @@ export default function ExerciseCard(props) {
         <p className="capitalize">{exercise.muscles.join("&")}</p>
       </div>
       <div className="flex flex-col bg-slate-950 rounded gap-2 ">
-        {exercise.description}
+        {exercise.description.split("___").map((val) => {
+          return <div className="text-sm">{val}</div>;
+        })}
       </div>
       <div className="grid grid-cols-2 sm:grid-cols-4 sm:place-items-center gap-2">
         {["reps", "rest", "tempo"].map((info) => {
